6f8792f12a0882ea19ebd4fb52c32806186a95a7
wasifdawar <m.wasifdawar@gmail.com>
2021-11-30 14:19:07 +0500

    Setting up home screen with Static Data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Homescreen from './screens/Homescreen';
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Homescreen/>
     </div>
   );
 }
