e98a040146f829dd248cb2e3fd06820d2bc7499d
wasifdawar <m.wasifdawar@gmail.com>
2021-12-07 19:04:55 +0500

    Feat: Changes 7-dec-2021

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import Navbar from "./components/Navbar";
 import Homescreen from "./screens/Homescreen";
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes,Route } from "react-router-dom";
+import Productescreen from "./screens/Productescreen";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/" component={Homescreen} exact />
-        </Routes>
-      </BrowserRouter> */}
-      <Homescreen />
+      <BrowserRouter>
+      <Routes>
+      <Route path='/' component={Homescreen} exact/>
+      <Route path='/product/:id' component={Productescreen}/>
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ src/screens/Productescreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Productescreen(){
+    return (
+        <div>
+            <h1>
+                Product Description Page
+            </h1>
+        </div>
+    )
+}
