15d63feb87187565a7797795516856aabf5cce27
wasifdawar <m.wasifdawar@gmail.com>
2021-12-08 16:15:37 +0500

    Feat: Product ID

--- src/screens/Productescreen.js
+++ src/screens/Productescreen.js
@@ -1,18 +1,23 @@
 import React from "react";
-import { useParams } from "react-router";
-
+import { useParams,match } from "react-router";
 import products from "../products";
 
-const Productescreen = (product) => {
+const Productescreen = ({product}) => {
   const { id } = useParams();
-  //const products = product.find((product) => product.id === id);
+   console.log("Product:",products.id)
   return (
     <div>
       <h1>Product Description Page</h1>
-      <h1>The Product Id is{id} </h1>
-      <h1>{product.name}</h1>
+      <h1>The Product Id is{id}  </h1>
+     
     </div>
   );
 };
 
 export default Productescreen;
+
+
+
+
+
+  
