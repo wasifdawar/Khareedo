4d0cd695fed67641b021349b24ad171fe8f81ed8
wasifdawar <m.wasifdawar@gmail.com>
2021-12-08 17:45:08 +0500

    Feat: Resolving Errors

--- src/screens/Productescreen.js
+++ src/screens/Productescreen.js
@@ -8,8 +8,36 @@ const Productescreen = ({product}) => {
    
   return (
     <div>
-      <h1>Product Description Page</h1>
-      <h1>The Product Id is{id} </h1>
+        <h1>
+            Product Description
+        </h1>
+      {/* <div className="row">
+          <div className="col-md-6">
+              <div className="card p-2 m-2">
+                  <h1>{product.name}</h1>
+                  <img src={product.image}  className="img-fluid m-3 bigimg"/>
+                  <p>{product.description}</p>
+              </div>
+              <div className="col-md-6 text-left">
+                  <div className="m-2">
+                    <h1>
+                        Price:{product.price}
+                    </h1>
+                    <hr/>
+                    <h1>
+                        Select Quantity:
+                    </h1>
+                    <select>{[...Array(product.countInStock).keys()].map((x,i)=>{})}
+                    return <option value={i+1}>{i+1}</option>
+                    </select>
+                    <hr/>
+                    <button className="btn btn-dark">
+                        ADD TO CART
+                    </button>
+                  </div>
+              </div>
+          </div>
+      </div> */}
      
     </div>
   );
